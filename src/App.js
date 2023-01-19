5b0c93274cc19c256200c6347ca9e14f401f6d02
Igor <ig.vp@hotmail.com>
2023-01-19 10:15:29 -0300

    done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import WhoIM from "./components/WhoIM/WhoIM";
 import About from "./components/About/About";
 function App() {
   return (
-    <body className="relative z-20 h-screen w-screen overflow-y-scroll bg-white overflow-x-hidden scrollbar-thin scrollbar-track-transparent scrollbar-thumb-gray-300 scrollbar-thumb-rounded-3xl">
+    <body className="relative z-20 h-screen w-screen overflow-y-scroll bg-white font-Inter overflow-x-hidden scrollbar-thin scrollbar-track-transparent scrollbar-thumb-gray-300 scrollbar-thumb-rounded-3xl">
       <Header />
       <div className="absolute top-0 left-0 w-full">
         <WhoIM />
